9ecce1e986d6eb09c4fd0bb125792735e618a884
jakub.wieczorek <jwieczorek@future-processing.com>
2020-10-22 19:44:43 +0200

    merge

--- src/kodilla/Main.java
+++ src/kodilla/Main.java
@@ -2,10 +2,6 @@
 public class Main {
 
     public static void main(String[] args) {
-<<<<<<< HEAD
 	    System.out.println("hello from branch master");
-=======
-	    System.out.println("hello from branch B");
->>>>>>> BRANCH_B
     }
 }
