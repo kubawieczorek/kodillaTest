66431e604276ec902ae26522f6458645d311bba8
jakub.wieczorek <jwieczorek@future-processing.com>
2020-10-22 18:54:23 +0200

    next commit

--- src/kodilla/Main.java
+++ src/kodilla/Main.java
@@ -3,6 +3,6 @@ package kodilla;
 public class Main {
 
     public static void main(String[] args) {
-	    System.out.println("hello in Branch A");
+	    System.out.println("hello in Branch A A A");
     }
 }
